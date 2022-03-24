fac93d56d2259c9c8a28c7ebd6574fa64e81a4fc
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-03-24 10:16:54 +0900

    배열 활용 연습 2

--- CodingTest2022/getMax_0324.c
+++ CodingTest2022/getMax_0324.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main (void) {
+int main(void) {
 	int arr[] = { 2, 5, 78, 43, -45, 68, 31, 100, 45, 23 };
 	int Max = 0;
 	int Min = 0;
@@ -12,13 +12,14 @@ int main (void) {
 			Max = arr[i];
 			maxtemp = i;
 		}
-		if (arr[i] < Min){
+		if (arr[i] < Min) {
 			Min = arr[i];
 			mintemp = i;
-	}
-		
-	printf("최대값 : %d 최솟값 : %d", Max, Min);
-	printf("최대값위치 : %d 최솟값위치 : %d", maxtemp+1, mintemp+1);
+		}
 
-	return 0;
+		printf("최대값 : %d 최솟값 : %d", Max, Min);
+		printf("최대값위치 : %d 최솟값위치 : %d", maxtemp + 1, mintemp + 1);
+
+		return 0;
+	}
 }
