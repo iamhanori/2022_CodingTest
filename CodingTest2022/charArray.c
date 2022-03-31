6b478662c01bd31b46ca2851f250696e41558851
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-03-31 10:08:30 +0900

    길이 구하고 문자열 복사

--- CodingTest2022/charArray.c
+++ CodingTest2022/charArray.c
@@ -3,21 +3,42 @@
 int main(void) {
 	//char color[5][10] = { "red", "green", "blue", "brown", "cyan" };
 	char s1[20] = { "coding test" };
+	char s2[20];
+
 	int i, j;
 	int cnt = 0;
 
-	while (s1[cnt] != '\0') { // 문자열 끝인 널문자가 나오기 전까지 반복
-		cnt++;
-	}
-	printf("문자열의 길이 : %d\n", cnt);
+	// 길이 구하기
+	//while (s1[cnt] != '\0') {
+	//	cnt++;
+	//}
 	
-	cnt = 0;
 	for (i = 0; i < 20; i++) {
-		if (s1[i] == 0)
+		if (s2[i] == 0) {
 			break;
-		cnt++;
+		}
+
+		s2[i] = s1[i]; 
+		
+		//if (s1[i] == 0) {
+		//	break;
+		//}
 	}
-	printf("문자열의 길이 : %d\n", cnt);
+
+	printf("복사본 : %s\n", s2);
+
+	//while (s1[cnt] != '\0') { // 문자열 끝인 널문자가 나오기 전까지 반복
+	//	cnt++;
+	//}
+	//printf("문자열의 길이 : %d\n", cnt);
+	//
+	//cnt = 0;
+	//for (i = 0; i < 20; i++) {
+	//	if (s1[i] == 0)
+	//		break;
+	//	cnt++;
+	//}
+	//printf("문자열의 길이 : %d\n", cnt);
 
 
 	//for (i = 0; i < 5; i++) {
