23b14e4d447102c6c589161f7daba5b9a85e1338
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-04-21 09:12:34 +0900

    구구단 예제

---
+++ CodingTest2022/ptr.c
@@ -0,0 +1,17 @@
+ptr.c
+
+#include <stdio.h>
+
+int main(void){
+	int number;
+	int* pNumber = NULL;
+	number = 5;
+	printf("number의 값 : %d\n", number);
+	
+	pNumber = &number;
+	*pNumber = *pNumber+5;
+
+	printf("number의 값 : %d %d %p\n", number, *pNumber, *pNumber);
+return 0;
+
+}
